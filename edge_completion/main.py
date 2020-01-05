fix f1 calculation

edge_completion/main.py
@@ -17,11 +17,11 @@ args = get_args()
 print('Loading data...')
 train_dataset = EdgeMangaDataset(args.data_path + 'train/', 'train.csv', height=args.height, width=args.width)
 val_dataset = EdgeMangaDataset(args.data_path + 'val/', 'val.csv', height=args.height, width=args.width)
-test_dataset = EdgeMangaDataset(args.data_path + 'test/', 'test.csv', height=args.height, width=args.width)
+#test_dataset = EdgeMangaDataset(args.data_path + 'test/', 'test.csv', height=args.height, width=args.width)
 
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.workers)
 val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.workers)
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.workers)
+#test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.workers)
 
 # Setup models
 print('\nInitializing models...')

edge_completion/test.py
@@ -18,11 +18,11 @@ def test(netG, loader, device, save_batches=0, save_path=None):
         save_images_outputs = []
 
         # Measure psrn and ssim on entire val set
-        for i, (images, masks, edge_inputs, edge_targets, _) in enumerate(loader):
-            images, masks, edge_inputs, edge_targets = images.to(device), masks.to(device), edge_inputs.to(device), edge_targets.to(device)
+        for i, (images, masks, text_masks, edge_inputs, edge_targets, _) in enumerate(loader):
+            images, masks, text_masks, edge_inputs, edge_targets = images.to(device), masks.to(device), text_masks.to(device), edge_inputs.to(device), edge_targets.to(device)
             
             # Pass images through generator
-            edge_outputs = netG(torch.cat((images, masks, edge_inputs), 1))
+            edge_outputs = netG(torch.cat((images, text_masks, edge_inputs), 1))
 
             # Get precision and recall
             precision, recall = edgeacc(edge_targets * masks, edge_outputs * masks)
@@ -35,7 +35,7 @@ def test(netG, loader, device, save_batches=0, save_path=None):
                 for j in range(edge_outputs.size(0)):
                     save_images_outputs.append(((edge_outputs[j].cpu().data.numpy().transpose(1, 2, 0))*255).astype('uint8')) # Generated edge map
                     save_images_targets.append(((edge_targets[j].cpu().data.numpy().transpose(1, 2, 0))*255).astype('uint8'))  # Corresponding corresponding ground truth edge map
-            
+            break
         avg_precision = np.mean(precisions)
         avg_recall = np.mean(recalls)
 

edge_completion/train.py
@@ -35,7 +35,7 @@ def train_gan(netG, netD, train_loader, val_loader, optimizerG, optimizerD,
                       'FM_losses': [],
                       'Precision': [], 
                       'Recall': [],
-                      'FM_val_losses': []}
+                      'F1': [],}
     start = time.time()
 
     print('\nStarting to train...')
@@ -101,7 +101,7 @@ def train_gan(netG, netD, train_loader, val_loader, optimizerG, optimizerD,
             if (i+1)%args.batch_log_rate == 0:
                 print('[Epoch {}/{}, Batch {}/{}] FM loss: {}'
                       .format(epoch, args.epochs, i+1, len(train_loader), np.mean(fm_losses)))
-            
+            break
         # Save model
         save_checkpoint({'epoch': epoch,
                          'G_state_dict': netG.state_dict(),
@@ -129,7 +129,8 @@ def train_gan(netG, netD, train_loader, val_loader, optimizerG, optimizerD,
 
         train_hist['Precision'].append(avg_precision)
         train_hist['Recall'].append(avg_recall)
-        print("Precision: {} Recall: {}\n".format(avg_precision, avg_recall))
+        train_hist['F1'].append((2*avg_precision*avg_recall) / (avg_precision+avg_recall))
+        print("Precision: {} Recall: {} F1: {}\n".format(avg_precision, avg_recall, (2*avg_precision*avg_recall) / (avg_precision+avg_recall)))
 
         # Save training history plot
         save_plots(train_hist, args.plot_path)